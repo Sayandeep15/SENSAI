d015185f755eab09eb3fee5b0044bbe1cb99ef6e
Sayandeep15 <sayandeepdutta83685@gmail.com>
2025-08-15 12:32:23 +0530

    commit

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import Header from "@/components/header";
+
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      home
-    </div>
+    <>
+    <Header/>
+    home
+    </>
+      
+    
   );
 }
