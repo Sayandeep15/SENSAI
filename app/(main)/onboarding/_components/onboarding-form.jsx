1183eb7c0e41b37779ba7a419b307b262ac9b3be
Sayandeep15 <sayandeepdutta83685@gmail.com>
2025-08-17 20:17:20 +0530

    commit

--- app/(main)/onboarding/_components/onboarding-form.jsx
+++ app/(main)/onboarding/_components/onboarding-form.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+'use client';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { onboardingSchema } from '@/lib/schema';
 
-const OnboardingForm = () => {
+const OnboardingForm = ({industries}) => {
+const router = useRouter();
+const [selectedIndustry, setSelectedIndustry] = useState(null);
+
+const {
+  register,
+  handleSubmit,
+  formState:{errors},
+  setValue,
+  watch
+}=useForm({resolver: zodResolver(onboardingSchema)});
+
   return (
     <div>OnboardingForm</div>
   )
