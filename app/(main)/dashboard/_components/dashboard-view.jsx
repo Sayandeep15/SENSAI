8c5e06f5a642baa5835f9d362b53aeca2e55a75e
Sayandeep15 <sayandeepdutta83685@gmail.com>
2025-08-18 18:09:15 +0530

    commit

--- app/(main)/dashboard/_components/dashboard-view.jsx
+++ app/(main)/dashboard/_components/dashboard-view.jsx
@@ -1,47 +1,56 @@
+"use client";
+import { format, formatDistanceToNow } from 'date-fns';
 import { LineChart, TrendingDown, TrendingUp } from 'lucide-react';
 import React from 'react'
 
-const DashboardView  = ({insights}) => {
+const DashboardView = ({ insights }) => {
     // Transform salary data for the chart
-  const salaryData = insights.salaryRanges.map((range) => ({
-    name: range.role,
-    min: range.min / 1000,
-    max: range.max / 1000,
-    median: range.median / 1000,
-  }));
+    const salaryData = insights.salaryRanges.map((range) => ({
+        name: range.role,
+        min: range.min / 1000,
+        max: range.max / 1000,
+        median: range.median / 1000,
+    }));
 
-const getDemandLevelColor = (level) => {
-    switch (level.toLowerCase()) {
-      case "high":
-        return "bg-green-500";
-      case "medium":
-        return "bg-yellow-500";
-      case "low":
-        return "bg-red-500";
-      default:
-        return "bg-gray-500";
-    }
-  };
-  const getDemgetMarketOutlookInfo  = (outlook) => {
-    switch (outlook.toLowerCase()) {
-      case "positive":
-        return {icon: TrendingUp ,color:"bg-green-500"};
-      case "neutral":
-        return {icon: LineChart ,color:"bg-yellow-500"};
-      case "negative":
-        return {icon: TrendingDown ,color:"bg-red-500"};
-      default:
-        return {icon: LineChart ,color:"bg-gray-500"};
-    }
-  };
-  const OutlookIcon =getDemgetMarketOutlookInfo(insights.marketOutlook).icon;
-  const outlookColor  =getDemgetMarketOutlookInfo(insights.marketOutlook).color
- 
+    const getDemandLevelColor = (level) => {
+        switch (level.toLowerCase()) {
+            case "high":
+                return "bg-green-500";
+            case "medium":
+                return "bg-yellow-500";
+            case "low":
+                return "bg-red-500";
+            default:
+                return "bg-gray-500";
+        }
+    };
+    const getDemgetMarketOutlookInfo = (outlook) => {
+        switch (outlook.toLowerCase()) {
+            case "positive":
+                return { icon: TrendingUp, color: "bg-green-500" };
+            case "neutral":
+                return { icon: LineChart, color: "bg-yellow-500" };
+            case "negative":
+                return { icon: TrendingDown, color: "bg-red-500" };
+            default:
+                return { icon: LineChart, color: "bg-gray-500" };
+        }
+    };
+    const OutlookIcon = getDemgetMarketOutlookInfo(insights.marketOutlook).icon;
+    const outlookColor = getDemgetMarketOutlookInfo(insights.marketOutlook).color
+
+    // Format dates using date-fns
+    const lastUpdatedDate = format(new Date(insights.lastUpdated), 'dd/mm/yyyy');
+    const nextUpdateDistance = formatDistanceToNow(
+        new Date(insights.nextUpdate),
+        { addSuffix: true }
+    );
 
 
-  return (
-    <div>DashboardView </div>
-  )
+
+    return (
+        <div>DashboardView </div>
+    )
 }
 
 export default DashboardView 
--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { getUserOnboardingStatus, updateUser } from '@/actions/user'
 import { redirect } from "next/navigation";
+import DashboardView from './_components/dashboard-view';
+import { getIndustryInsights } from '@/actions/dashboard';
 
 const Dashboard = async () => {
+  const insights = await getIndustryInsights();
+  
   const { isOnboarded } = await getUserOnboardingStatus();
   if (!isOnboarded) {
     redirect('/onboarding');
   }
   return (
-    <div>Dashboard</div>
+    <DashboardView insights={insights}/>
   )
 }
 
