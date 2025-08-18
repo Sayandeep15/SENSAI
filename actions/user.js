1d550a36de39575a7eb4c4f47c4043de34e7bc82
Sayandeep15 <sayandeepdutta83685@gmail.com>
2025-08-18 14:07:21 +0530

    commit

--- actions/user.js
+++ actions/user.js
@@ -1,74 +1,69 @@
 "use server"
 import { auth } from "@clerk/nextjs/server";
 import { db } from "@/lib/prisma";
+import { generateAIInsights } from "./dashboard";
 import { success } from "zod";
 
 
 export async function updateUser(data) {
-    //CHECK IF THE USER LOGGED IN OR NOT
-    const { userId } = await auth();
-    if (!userId) throw new Error("Unauthorized");
+  //CHECK IF THE USER LOGGED IN OR NOT
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
 
-    //CHECK USER DATA IS PRESENT IN DB OR NOT
-    const user = await db.user.findUnique({
-        where: { clerkUserId: userId }
-    });
-    if (!user) throw new Error("User not found");
+  //CHECK USER DATA IS PRESENT IN DB OR NOT
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId }
+  });
+  if (!user) throw new Error("User not found");
 
-    try {
-        //START A TRANSACTION TO HANDLE SOME OPERATION
-        const result = await db.$transaction(
-            async (tx) => {
+  try {
+    //START A TRANSACTION TO HANDLE SOME OPERATION
+    const result = await db.$transaction(
+      async (tx) => {
 
-                // CHECK IF INSDUSTRY EXIST OR 
-                let industryInsight = await tx.industryInsight.findUnique({
-                    where: { industry: data.industry },
-                });
+        // CHECK IF INSDUSTRY EXIST OR 
+        let industryInsight = await tx.industryInsight.findUnique({
+          where: { industry: data.industry },
+        });
 
-                // IF NOT EXIST THEN CREATE DEFAULT INSDUSTRY
-                if (!industryInsight) {
-                    industryInsight = await tx.industryInsight.create({
-                        data: {
-                            industry: data.industry,
-                            salaryRanges: [],
-                            growthRate: 0,
-                            demandLevel: "MEDIUM",
-                            topSkills: [],
-                            marketOutlook: "NUTRAL",
-                            keyTrends: [],
-                            recommendedSkills: [],
-                            nextUpdate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        // IF NOT EXIST THEN CREATE DEFAULT INSDUSTRY
+        if (!industryInsight) {
+          const insights = await generateAIInsights(data.industry);
 
-                        },
-                    });
-                }
+          industryInsight = await db.industryInsight.create({
+            data: {
+              industry: data.industry,
+              ...insights,
+              nextUpdate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+            },
+          });
+        }
+        // UPDATE USER INSDUSTRY
+        const updatedUser = await tx.user.update({
+          where: {
+            id: user.id,
+          },
+          data: {
+            industry: data.industry,
+            experience: data.experience,
+            bio: data.bio,
+            skills: data.skills,
+          },
+        });
 
-                // UPDATE USER INSDUSTRY
-                const updatedUser = await tx.user.update({
-                    where: {
-                        id: user.id,
-                    },
-                    data: {
-                        industry: data.industry,
-                        experience: data.experience,
-                        bio: data.bio,
-                        skills: data.skills,
-                    },
-                });
+        return (updatedUser, industryInsight);
 
-                return (updatedUser, industryInsight);
+      },//tx
+      {
+        timeout: 10000, // default: 5000
+      }
 
-            },//tx
-            {
-                timeout: 10000, // default: 5000
-            }
-
-        );//transaction
-        return {success:true, ...result};
-    } catch (error) {
-        console.error("Error updating user and industry:", error.message);
-        throw new Error("Failed to update profile" +error.message);
-    }
+    );//transaction
+    return { success: true, ...result };
+  } catch (error) {
+    console.error("Error updating user and industry:", error.message);
+    throw new Error("Failed to update profile" + error.message);
+  }
 
 }
 
--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { getUserOnboardingStatus, updateUser } from '@/actions/user'
+import { redirect } from "next/navigation";
 
 const Dashboard = async () => {
   const { isOnboarded } = await getUserOnboardingStatus();
