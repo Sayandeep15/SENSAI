5cd99f6742f74b71dfe8cf6547bb088975d374f1
Sayandeep15 <sayandeepdutta83685@gmail.com>
2025-10-03 19:49:26 +0530

    commit

--- hooks/use-fetch.js
+++ hooks/use-fetch.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { toast } from "sonner";
 
-const useFecth = (cb) => {
+// it will take a  callback function as a parameter and it will return data, loading, error, and a function to call the api
+
+const useFecth = (callBack) => {
     const [data, setData] = useState(undefined);
     const [loading, setLoading] = useState(null);
     const [error, setError] = useState(null);
@@ -10,7 +12,7 @@ const useFecth = (cb) => {
         setLoading(true);
         setError(null)
         try {
-            const response = await cb(...args)
+            const response = await callBack(...args)
             setData(response)
             setError(null)
         } catch (error) {
