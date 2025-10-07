ab32f5e4e982738ad96e05031fcf2eb1b3841ac3
Sayandeep15 <sayandeepdutta83685@gmail.com>
2025-10-07 13:24:15 +0530

    commit

--- app/(main)/interview/_components/scrollToTopButton.jsx
+++ app/(main)/interview/_components/scrollToTopButton.jsx
@@ -20,18 +20,18 @@ export default function ScrollToTopButton() {
   return (
     <button
       onClick={scrollToTop}
-      className={`fixed bottom-6 right-6 z-50 group flex items-center gap-2
-        rounded-full transition-all duration-300 ease-in-out
+      className={`fixed bottom-6 right-6 z-50 group flex items-center 
+        rounded-full transition-all duration-300 ease-in-out hover:gap-1
         ${visible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-4"}
-        bg-zinc-700 hover:bg-zinc-600 text-white shadow-inner hover:shadow-[0_0_15px_#66ccff]
-        h-10 px-3`}
+        bg-white  text-black shadow-inner 
+        h-10 px-2.5`}
       aria-label="Back to top"
     >
       <ArrowUp
         className="w-5 h-5 transition-transform duration-300 group-hover:-translate-y-1 flex-shrink-0"
       />
       <span
-        className="whitespace-nowrap opacity-0 max-w-0 group-hover:opacity-100 group-hover:max-w-[100px]
+        className="whitespace-nowrap opacity-0 max-w-0 group-hover:opacity-100 group-hover:max-w-[100px] 
         transition-all duration-300 overflow-hidden text-sm font-medium"
       >
         Back to top
