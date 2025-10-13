8615b9aad33cdcb8bf4028bf0639134b9cde86f1
Sayandeep15 <sayandeepdutta83685@gmail.com>
2025-10-13 18:58:02 +0530

    commit

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,5 +1,21 @@
+// import { SignIn } from "@clerk/nextjs";
+
+// export default function Page() {
+//   return <SignIn />;
+// }
+
+import { useAuth } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 import { SignIn } from "@clerk/nextjs";
 
-export default function Page() {
+export default function SignInPage() {
+  const { isSignedIn } = useAuth();
+  const router = useRouter();
+
+  if (isSignedIn) {
+    router.push("/dashboard"); // or your custom route
+    return null;
+  }
+
   return <SignIn />;
 }
