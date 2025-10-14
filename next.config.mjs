a2dfd7736aaa58266d08703a5b7170032a2b4feb
Sayandeep15 <sayandeepdutta83685@gmail.com>
2025-10-14 20:00:31 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,17 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "randomuser.me",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -5,9 +19,12 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "randomuser.me",
+        port: "",                 // leave blank (important on Vercel)
+        pathname: "/api/portraits/**", // add this to allow all portrait images
       },
     ],
   },
 };
 
 export default nextConfig;
+
