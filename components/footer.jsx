048ee7105bbe1673e728a6832473615e0921b903
Sayandeep15 <sayandeepdutta83685@gmail.com>
2025-10-16 18:35:16 +0530

    commit

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
-import { ImGithub } from "react-icons/im";
-import { FaLinkedinIn } from "react-icons/fa6";
+import { Mail, Linkedin} from "lucide-react";
 import { FaRegCopyright } from "react-icons/fa6";
 
 
 
 const Footer = () => {
   return (
-    <div className=' border-t bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60 w-full text-white  mt-4  flex justify-around py-4'>
-        <p className='text-xs flex items-center gap-1'><FaRegCopyright />2025 | Developed by Sayandeep Dutta  </p>
-        <ul className='flex items-center gap-4 font-semibold cursor-pointer '>
-            <li className=' hover:scale-110 transition-all ease-in '><a className=' ' target='_blank' href="https://github.com/Sayandeep15/Password-Manager"><ImGithub /></a></li>
-            <li className=' hover:scale-110 transition-all ease-in '><a className=' ' target='_blank' href="https://www.linkedin.com/in/sayandeep-dutta-profile/"><FaLinkedinIn /></a></li>
-        </ul>
+    <div className=' border-t bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60 w-full text-muted-foreground  mt-4  flex justify-around py-4'>
+      <p className='text-xs flex items-center gap-1'><FaRegCopyright />2025 | Developed by Sayandeep Dutta  </p>
+      <ul className='flex items-center gap-3 font-semibold cursor-pointer '>
+        <li className="hover:scale-110 transition-all ease-in">
+          <a
+            href="mailto:sayandeepdutta83685@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className=""
+          >
+           <Mail className="h-4 w-4 text-muted-foreground"/>
+          </a>
+        </li>
+        <li className=' hover:scale-110 transition-all ease-in '><a className=' ' target='_blank' href="https://www.linkedin.com/in/sayandeep-dutta-profile/"><Linkedin  className="h-4 w-4 text-muted-foreground"/></a></li>
+      </ul>
     </div>
   )
 }
